bb20cbfb8e6e57c59f31f9a0acdd66c5d9c9c969
charan51 <cvcharan51@gmail.com>
2018-10-28 15:39:19 +0530

    Added accsignment 7.2

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Using for loop');
+for(var i=5; i<=50; i+=5) {
+    console.log(i);
+}
+console.log('Using while loop');
+var start = 5;
+while(start <=50) {
+    console.log(start);
+    start+=5;
+}
